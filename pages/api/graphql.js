207abd9c39e6d32d64d902a036cf75d009e69e3d
adminmojo <adesh.mvc@gmail.com>
2023-02-10 17:49:16 +0530

    GraphQL setup

---
+++ pages/api/graphql.js
@@ -0,0 +1,33 @@
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+
+import typeDefs from '../../db/schema';
+import resolvers from "../../db/resolvers";
+import connectDb from "../../db/config"
+connectDb();
+// const resolvers = {
+//     Query: {
+//         hello: () => 'world',
+//         book: () => 'C#'
+//     },
+// };
+
+// const typeDefs = gql`
+//   type Query {
+//     hello: String
+//   }
+//   type Query{
+//     book:String
+//   }
+// `;
+
+const server = new ApolloServer({
+    resolvers,
+    typeDefs,
+    introspection: true,
+    playground: true,
+});
+
+export default startServerAndCreateNextHandler(server);
+
+// https://github.com/apollo-server-integrations/apollo-server-integration-next
