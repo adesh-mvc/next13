aafa60353eb507b2cb502ee35083b4b8648c56d1
adminmojo <adesh.mvc@gmail.com>
2023-05-03 19:04:31 +0530

    checkbox group

--- app/admin/products/[...product]/TestCheckbox copy.jsx
+++ app/admin/products/[...product]/TestCheckbox copy.jsx
@@ -1,84 +1,223 @@
 "use client"
 import React, { useState, useEffect } from "react";
 const TestCheckBox = () => {
-    const [checkedAll, setCheckedAll] = useState(false);
-    const [checked, setChecked] = useState({
-        nr1: false,
-        nr2: false
-    });
+    const [checkedOne, setCheckedOne] = useState(false);
+    const [checkedTwo, setCheckedTwo] = useState(false);
+    const [rootData, setRootData] = useState([]);
 
-    /* ################################################ */
-    /* #### TOGGLES checK STATE BASED ON inputName #### */
-    /* ################################################ */
-
-    const toggleCheck = (inputName) => {
-        setChecked((prevState) => {
-            const newState = { ...prevState };
-            newState[inputName] = !prevState[inputName];
-            return newState;
+    const selectAllOne = (e) => {
+        const { name, checked } = e.target;
+        const isOk = checked;
+        setCheckedOne(isOk);
+        // console.log(checkedOne);
+        document.getElementsByClassName(name).forEach((row, index) => {
+            setRootData(result => ({ ...result, [row.name]: isOk, [`${row.name}_service`]: { ip: isOk, op: isOk } }));
+            //console.log('rootData:', rootData);
         });
-    };
+    }
+    const toggleCheck = (e) => {
+        const { name, checked } = e.target;
+        // console.log('nodeName:', e.target.name, e.target.checked);
+        setRootData(result => ({ ...result, [name]: checked, [`${name}_service`]: { ip: checked, op: checked } }));
+        rootSetter(document.getElementsByClassName('root_1'));
+        console.log('Toggledata:', rootData);
+    }
+    const toggleLeaf = (e) => {
+        const { name } = e.target;
+        const service = {};
+        console.log('rootData:', service);
+        var uncheck = 0;
+        const group_name = document.getElementsByClassName(name);
+        group_name.forEach((row, index) => {
+            const leafOk = row.checked;
+            if (!leafOk) { uncheck++ }
 
-    /* ###################################################### */
-    /* #### CHECKS OR UNCHECKS ALL FROM SELECT ALL CLICK #### */
-    /* ###################################################### */
-
-    const selectAll = (value) => {
-        setCheckedAll(value);
-        setChecked((prevState) => {
-            const newState = { ...prevState };
-            for (const inputName in newState) {
-                newState[inputName] = value;
-            }
-            return newState;
+            service[row.getAttribute('data-service')] = row.checked;
         });
-    };
-
-    /* ############################################# */
-    /* #### EFFECT TO CONTROL CHECKED_ALL STATE #### */
-    /* ############################################# */
-
-    // IF YOU CHECK BOTH INDIVIDUALLY. IT WILL ACTIVATE THE checkedAll STATE
-    // IF YOU UNCHECK ANY INDIVIDUALLY. IT WILL DE-ACTIVATE THE checkAll STATE
-
-    useEffect(() => {
-        let allChecked = true;
-        for (const inputName in checked) {
-            if (checked[inputName] === false) {
-                allChecked = false;
-            }
-        }
-        if (allChecked) {
-            setCheckedAll(true);
+        if (group_name.length == uncheck) {
+            rootSetter(document.getElementsByClassName('root_1'));
+            setRootData(result => ({ ...result, [name]: false }));
         } else {
-            setCheckedAll(false);
+            setRootData(result => ({ ...result, [name]: true }));
+            setCheckedOne(true);
         }
-    }, [checked]);
+        setRootData(result => ({ ...result, [`${name}_service`]: { ip: service.ip, op: service.op } }));
+    }
+    const rootSetter = (rootChild) => {
+        var totalchecked = 0;
+        var totalunchecked = rootChild.length;
+        rootChild.forEach((row) => {
+            if (row.checked) {
+                totalchecked++;
+            } else {
+                totalunchecked--;
+            }
+        });
+        console.log("totalunchecked:", totalunchecked, rootChild.length);
+        if (!totalunchecked) {
+            setCheckedOne(false);
+        }
+        if (totalchecked) {
+            setCheckedOne(true);
+        }
+    }
+
+
     return (
         <div className="App">
             <div>
-                <label>All</label>
+
                 <input
                     type="checkbox"
-                    onChange={(event) => selectAll(event.target.checked)}
-                    checked={checkedAll}
+                    onChange={selectAllOne}
+                    checked={checkedOne}
+                    name="root_1"
                 />
+                <label>Root 1</label>
             </div>
             <ul>
                 <li>
 
                     <input
                         type="checkbox"
-                        name="nr1"
-                        onChange={() => toggleCheck("nr1")}
-                        checked={checked["nr1"]}
+                        className="root_1"
+                        name="One"
+
+                        onChange={(event) => toggleCheck(event)}
+                        checked={rootData.One}
                     />
-                    <label>1</label>
+                    <label> R 1 1</label>
                     <ul>
                         <li>
                             <input
+                                className="One"
                                 type="checkbox"
-                                name="nr1"
+                                name="One"
+                                data-service={`ip`}
+                                onChange={(event) => toggleLeaf(event)}
+                                checked={rootData.One_service?.ip}
+                            />
+                            <label>One of one</label>
+
+                        </li>
+                        <li>
+                            <input
+                                className="One"
+                                type="checkbox"
+                                name="One"
+                                data-service={`op`}
+                                onChange={(event) => toggleLeaf(event)}
+                                checked={rootData.One_service?.op}
+                            />
+                            <label>One of two</label>
+
+
+                        </li>
+                    </ul>
+                </li>
+                <li>
+
+                    <input
+                        type="checkbox"
+                        className="root_1"
+                        name="Two"
+                        onChange={(e) => toggleCheck(e)}
+                        checked={rootData.Two}
+                    />
+                    <label> R 1 2</label>
+                    <ul>
+                        <li>
+                            <input
+                                className="Two"
+                                type="checkbox"
+                                name="Two"
+                                data-service={`ip`}
+                                onChange={(e) => toggleLeaf(e)}
+                                checked={rootData.Two_service?.ip}
+                            />
+                            <label>1 2 of one</label>
+
+                        </li>
+                        <li>
+                            <input
+                                className="Two"
+                                type="checkbox"
+                                name="Two"
+                                data-service={`op`}
+                                onChange={(e) => toggleLeaf(e)}
+                                checked={rootData.Two_service?.op}
+                            />
+                            <label>1 2 of two</label>
+
+
+                        </li>
+                    </ul>
+                </li>
+                <li>
+
+                    <input
+                        type="checkbox"
+                        className="root_1"
+                        name="Three"
+                        onChange={(e) => toggleCheck(e)}
+                        checked={rootData.Three}
+                    />
+                    <label> R 1 3</label>
+                    <ul>
+                        <li>
+                            <input
+                                className="Three"
+                                type="checkbox"
+                                name="Three"
+                                data-service={`ip`}
+                                onChange={(e) => toggleLeaf(e)}
+                                checked={rootData.Three_service?.ip}
+                            />
+                            <label>1 3 of one</label>
+
+                        </li>
+                        <li>
+                            <input
+                                className="Three"
+                                type="checkbox"
+                                name="Three"
+                                data-service={`op`}
+                                onChange={(e) => toggleLeaf(e)}
+                                checked={rootData.Three_service?.op}
+                            />
+                            <label>1 3 of two</label>
+
+
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <div>
+
+                <input
+                    type="checkbox"
+                /*   onChange={(event) => selectAll(event.target.checked)} */
+                /*   checked={checkedAll} */
+                />
+                <label>Root 2</label>
+            </div>
+            <ul>
+                <li>
+
+                    <input
+                        type="checkbox"
+                        className="root_2"
+                        name="One"
+                    /* onChange={() => toggleCheck("nr1")} */
+                    /* checked={checked["nr1"]} */
+                    />
+                    <label> R 1 1</label>
+                    <ul>
+                        <li>
+                            <input
+                                className=""
+                                type="checkbox"
+                                name="One1"
 
                             />
                             <label>One of one</label>
@@ -87,7 +226,7 @@ const TestCheckBox = () => {
                         <li>
                             <input
                                 type="checkbox"
-                                name="nr1"
+                                name="One2"
 
                             />
                             <label>One of two</label>
@@ -100,28 +239,63 @@ const TestCheckBox = () => {
 
                     <input
                         type="checkbox"
-                        name="nr2"
-                        onChange={() => toggleCheck("nr2")}
-                        checked={checked["nr2"]}
+                        className="root_2"
+                        name="Two"
+                    /* onChange={() => toggleCheck("nr1")} */
+                    /* checked={checked["nr1"]} */
                     />
-                    <label>2</label>
+                    <label> R 1 2</label>
                     <ul>
                         <li>
                             <input
+                                className=""
                                 type="checkbox"
-                                name="nr1"
+                                name="One1"
 
                             />
-                            <label>Two of one</label>
+                            <label>One of one</label>
 
                         </li>
                         <li>
                             <input
                                 type="checkbox"
-                                name="nr1"
+                                name="One2"
 
                             />
-                            <label>Two of two</label>
+                            <label>One of two</label>
+
+
+                        </li>
+                    </ul>
+                </li>
+                <li>
+
+                    <input
+                        type="checkbox"
+                        className="root_2"
+                        name="Three"
+                    /* onChange={() => toggleCheck("nr1")} */
+                    /* checked={checked["nr1"]} */
+                    />
+                    <label> R 1 3</label>
+                    <ul>
+                        <li>
+                            <input
+                                className="root_2"
+                                type="checkbox"
+                                name="One3"
+
+                            />
+                            <label>One of one</label>
+
+                        </li>
+                        <li>
+                            <input
+                                type="checkbox"
+                                name="One2"
+
+                            />
+                            <label>One of two</label>
 
 
                         </li>
--- app/admin/products/[...product]/TestCheckbox.jsx
+++ app/admin/products/[...product]/TestCheckbox.jsx
@@ -1,14 +1,22 @@
 "use client"
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef } from "react";
+
+
+const rootSet = ["root_1", "root_2"];
+const rootGroup = ["R1", "R2", "R3"];
 const TestCheckBox = () => {
-    const [checkedOne, setCheckedOne] = useState(false);
+    const root = useRef(false);
+    const root_group = useRef(false);
+
+    const [checkedOne, setCheckedOne] = useState({ root_1: false, root_2: false });
     const [checkedTwo, setCheckedTwo] = useState(false);
-    const [rootData, setRootData] = useState([]);
+    const [rootData, setRootData] = useState({ R1: false, R1_service: { ip: false, op: false }, R2: false, R2_service: { ip: false, op: false }, R3: false, R3_service: { ip: false, op: false } });
+
 
     const selectAllOne = (e) => {
         const { name, checked } = e.target;
         const isOk = checked;
-        setCheckedOne(isOk);
+        setCheckedOne(rootRes => ({ ...rootRes, [name]: isOk }));
         // console.log(checkedOne);
         document.getElementsByClassName(name).forEach((row, index) => {
             setRootData(result => ({ ...result, [row.name]: isOk, [`${row.name}_service`]: { ip: isOk, op: isOk } }));
@@ -20,22 +28,40 @@ const TestCheckBox = () => {
         // console.log('nodeName:', e.target.name, e.target.checked);
         setRootData(result => ({ ...result, [name]: checked, [`${name}_service`]: { ip: checked, op: checked } }));
         rootSetter(document.getElementsByClassName('root_1'));
+        console.log('Toggledata:', rootData);
     }
     const toggleLeaf = (e) => {
-        console.log('nodeName:', e.target.name, e.target.checked);
-        const { name, checked } = e.target;
-        setRootData(result => ({ ...result, [`One_service`]: { ip: checked, op: checked } }));
+        const { name } = e.target;
+        const service = {};
+        console.log('rootData:', service);
+        var uncheck = 0;
+        const group_name = document.getElementsByClassName(name);
+        group_name.forEach((row, index) => {
+            const leafOk = row.checked;
+            if (!leafOk) { uncheck++ }
+
+            service[row.getAttribute('data-service')] = row.checked;
+        });
+        if (group_name.length == uncheck) {
+            rootSetter(document.getElementsByClassName('root_1'));
+            setRootData(result => ({ ...result, [name]: false }));
+        } else {
+            setRootData(result => ({ ...result, [name]: true }));
+            setCheckedOne(true);
+        }
+        setRootData(result => ({ ...result, [`${name}_service`]: { ip: service.ip, op: service.op } }));
     }
     const rootSetter = (rootChild) => {
         var totalchecked = 0;
         var totalunchecked = rootChild.length;
-        rootChild.forEach((row, index) => {
+        rootChild.forEach((row) => {
             if (row.checked) {
                 totalchecked++;
             } else {
                 totalunchecked--;
             }
         });
+        console.log("totalunchecked:", totalunchecked, rootChild.length);
         if (!totalunchecked) {
             setCheckedOne(false);
         }
@@ -43,232 +69,91 @@ const TestCheckBox = () => {
             setCheckedOne(true);
         }
     }
+    // root 2
+
+    const rootHandler = () => {
+        const { name } = root.current
+        // console.log(name)
+        document.getElementsByClassName(name).forEach((row, index) => {
+
+            // setRootData(result => ({ ...result, [row.name]: isOk, [`${row.name}_service`]: { ip: isOk, op: isOk } }));
+            console.log('rootData:', row.checked);
+        });
+    }
+
     return (
         <div className="App">
-            <div>
+            {rootSet.map((set, o) => {
+                return (<>
 
-                <input
-                    type="checkbox"
-                    onChange={selectAllOne}
-                    checked={checkedOne}
-                    name="root_1"
-                />
-                <label>Root 1</label>
-            </div>
-            <ul>
-                <li>
-
-                    <input
-                        type="checkbox"
-                        className="root_1"
-                        name="One"
-
-                        onChange={(event) => toggleCheck(event)}
-                        checked={rootData.One}
-                    />
-                    <label> R 1 1</label>
+                    <div>
+                        <input
+                            type="checkbox"
+                            onChange={selectAllOne}
+                            checked={checkedOne[set]}
+                            name={set}
+                        />
+                        <label>Root {o + 1}</label>
+                    </div>
                     <ul>
-                        <li>
-                            <input
-                                className=""
-                                type="checkbox"
-                                name="One1"
-                                onChange={(event) => toggleLeaf(event)}
-                                checked={rootData.One_service?.ip}
-                            />
-                            <label>One of one</label>
+                        {rootGroup.map((group, g) => {
+                            console.log("group", group)
+                            console.log('rootData.R1', `${rootData.group}`)
+                            return (<>
+                                <li>
+                                    <span>qwewewe{group}</span>
+                                    <input
+                                        type="checkbox"
+                                        className={set}
+                                        name={`${group}`}
 
-                        </li>
-                        <li>
-                            <input
-                                type="checkbox"
-                                name="One2"
-                                checked={rootData.One_service?.op}
-                            />
-                            <label>One of two</label>
+                                        onChange={(event) => toggleCheck(event)}
+                                        checked={rootData.R1}
+                                    />
+                                    <label> R {o + 1} {g + 1}</label>
+                                    <ul>
+                                        <li>
+                                            <input
+                                                className="One"
+                                                type="checkbox"
+                                                name="One"
+                                                data-service={`ip`}
+                                                onChange={(event) => toggleLeaf(event)}
+                                                checked={rootData.One_service?.ip}
+                                            />
+                                            <label>One of one</label>
+
+                                        </li>
+                                        <li>
+                                            <input
+                                                className="One"
+                                                type="checkbox"
+                                                name="One"
+                                                data-service={`op`}
+                                                onChange={(event) => toggleLeaf(event)}
+                                                checked={rootData.One_service?.op}
+                                            />
+                                            <label>One of two</label>
 
 
-                        </li>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </>)
+                        })}
+
                     </ul>
-                </li>
-                <li>
-
-                    <input
-                        type="checkbox"
-                        className="root_1"
-                        name="Two"
-                        onChange={(e) => toggleCheck(e)}
-                        checked={rootData.Two}
-                    />
-                    <label> R 1 2</label>
-                    <ul>
-                        <li>
-                            <input
-                                className=""
-                                type="checkbox"
-                                name="One1"
-                                onChange={(e) => toggleLeaf(e)}
-                                checked={rootData.Two_service?.ip}
-                            />
-                            <label>One of one</label>
-
-                        </li>
-                        <li>
-                            <input
-                                type="checkbox"
-                                name="One2"
-                                checked={rootData.Two_service?.op}
-                            />
-                            <label>One of two</label>
+                </>)
+            })}
 
 
-                        </li>
-                    </ul>
-                </li>
-                <li>
-
-                    <input
-                        type="checkbox"
-                        className="root_1"
-                        name="Three"
-                        onChange={(e) => toggleCheck(e)}
-                        checked={rootData.Three}
-                    />
-                    <label> R 1 3</label>
-                    <ul>
-                        <li>
-                            <input
-                                className=""
-                                type="checkbox"
-                                name="One3"
-                                checked={rootData.Three_service?.ip}
-                            />
-                            <label>One of one</label>
-
-                        </li>
-                        <li>
-                            <input
-                                type="checkbox"
-                                name="One2"
-                                checked={rootData.Three_service?.op}
-                            />
-                            <label>One of two</label>
 
 
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-            <div>
-
-                <input
-                    type="checkbox"
-                /*   onChange={(event) => selectAll(event.target.checked)} */
-                /*   checked={checkedAll} */
-                />
-                <label>Root 2</label>
-            </div>
-            <ul>
-                <li>
-
-                    <input
-                        type="checkbox"
-                        className="root_2"
-                        name="One"
-                    /* onChange={() => toggleCheck("nr1")} */
-                    /* checked={checked["nr1"]} */
-                    />
-                    <label> R 1 1</label>
-                    <ul>
-                        <li>
-                            <input
-                                className=""
-                                type="checkbox"
-                                name="One1"
-
-                            />
-                            <label>One of one</label>
-
-                        </li>
-                        <li>
-                            <input
-                                type="checkbox"
-                                name="One2"
-
-                            />
-                            <label>One of two</label>
 
 
-                        </li>
-                    </ul>
-                </li>
-                <li>
-
-                    <input
-                        type="checkbox"
-                        className="root_2"
-                        name="Two"
-                    /* onChange={() => toggleCheck("nr1")} */
-                    /* checked={checked["nr1"]} */
-                    />
-                    <label> R 1 2</label>
-                    <ul>
-                        <li>
-                            <input
-                                className=""
-                                type="checkbox"
-                                name="One1"
-
-                            />
-                            <label>One of one</label>
-
-                        </li>
-                        <li>
-                            <input
-                                type="checkbox"
-                                name="One2"
-
-                            />
-                            <label>One of two</label>
 
 
-                        </li>
-                    </ul>
-                </li>
-                <li>
 
-                    <input
-                        type="checkbox"
-                        className="root_2"
-                        name="Three"
-                    /* onChange={() => toggleCheck("nr1")} */
-                    /* checked={checked["nr1"]} */
-                    />
-                    <label> R 1 3</label>
-                    <ul>
-                        <li>
-                            <input
-                                className="root_2"
-                                type="checkbox"
-                                name="One3"
-
-                            />
-                            <label>One of one</label>
-
-                        </li>
-                        <li>
-                            <input
-                                type="checkbox"
-                                name="One2"
-
-                            />
-                            <label>One of two</label>
-
-
-                        </li>
-                    </ul>
-                </li>
-            </ul>
         </div>
     );
 }
