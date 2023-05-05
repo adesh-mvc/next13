c2f455ea4c5e016d7193c6bc4c0c26549d55efa4
adminmojo <adesh.mvc@gmail.com>
2023-05-05 18:51:15 +0530

    group checkbox

--- app/admin/products/[...product]/TestCheckbox.jsx
+++ app/admin/products/[...product]/TestCheckbox.jsx
@@ -3,31 +3,45 @@ import React, { useState, useRef } from "react";
 
 
 const rootSet = ["root_1", "root_2"];
-const rootGroup = ["R1", "R2", "R3"];
+const rootGroup = [{ "root_1": ["R1", "R2", "R3"] }, { "root_2": ["R1", "R2", "R3"] }];
+// const rootGroup = { R1_root_1: false, R2_root_1: false, R3_root_1: false };
 const TestCheckBox = () => {
     const root = useRef(false);
     const root_group = useRef(false);
 
     const [checkedOne, setCheckedOne] = useState({ root_1: false, root_2: false });
     const [checkedTwo, setCheckedTwo] = useState(false);
-    const [rootData, setRootData] = useState({ R1: false, R1_service: { ip: false, op: false }, R2: false, R2_service: { ip: false, op: false }, R3: false, R3_service: { ip: false, op: false } });
+    const [rootData, setRootData] = useState({
+        R1_root_1: false,
+        R1_root_1_service: { ip: false, op: false },
+        R2_root_1: false,
+        R2_root_1_service: { ip: false, op: false },
+        R3_root_1: false,
+        R3_root_1_service: { ip: false, op: false },
+        R1_root_2: false,
+        R1_root_2_service: { ip: false, op: false },
+        R2_root_2: false,
+        R2_root_2_service: { ip: false, op: false },
+        R3_root_2: false,
+        R3_root_2_service: { ip: false, op: false }
+    })
 
 
     const selectAllOne = (e) => {
         const { name, checked } = e.target;
         const isOk = checked;
         setCheckedOne(rootRes => ({ ...rootRes, [name]: isOk }));
-        // console.log(checkedOne);
+
         document.getElementsByClassName(name).forEach((row, index) => {
-            setRootData(result => ({ ...result, [row.name]: isOk, [`${row.name}_service`]: { ip: isOk, op: isOk } }));
-            console.log('rootData:', rootData);
+            setRootData(result => ({ ...result, [row.id]: isOk, [`${row.id}_service`]: { ip: isOk, op: isOk } }));
         });
     }
     const toggleCheck = (e) => {
-        const { name, checked } = e.target;
-        // console.log('nodeName:', e.target.name, e.target.checked);
-        setRootData(result => ({ ...result, [name]: checked, [`${name}_service`]: { ip: checked, op: checked } }));
-        rootSetter(document.getElementsByClassName('root_1'));
+        const { name, id, checked } = e.target;
+        console.log('nodeName:', id);
+        // const hasBoolean:checked?true:false
+        setRootData(result => ({ ...result, [id]: checked, [`${id}_service`]: { ip: checked, op: checked } }));
+        rootSetter(document.getElementsByClassName(name));
         console.log('Toggledata:', rootData);
     }
     const toggleLeaf = (e) => {
@@ -40,7 +54,7 @@ const TestCheckBox = () => {
             const leafOk = row.checked;
             if (!leafOk) { uncheck++ }
 
-            service[row.getAttribute('data-service')] = row.checked;
+            service[row.id] = row.checked;
         });
         if (group_name.length == uncheck) {
             rootSetter(document.getElementsByClassName('root_1'));
@@ -58,16 +72,19 @@ const TestCheckBox = () => {
             if (row.checked) {
                 totalchecked++;
             } else {
+
                 totalunchecked--;
             }
         });
-        console.log("totalunchecked:", totalunchecked, rootChild.length);
-        if (!totalunchecked) {
-            setCheckedOne(false);
-        }
         if (totalchecked) {
             setCheckedOne(true);
+            console.log("totalChecked:", totalchecked, rootChild.length);
         }
+        if (!totalunchecked) {
+            console.log("totalunchecked:", totalunchecked, rootChild.length);
+            setCheckedOne(false);
+        }
+
     }
     // root 2
 
@@ -86,7 +103,7 @@ const TestCheckBox = () => {
             {rootSet.map((set, o) => {
                 return (<>
 
-                    <div>
+                    <div key={o}>
                         <input
                             type="checkbox"
                             onChange={selectAllOne}
@@ -96,19 +113,19 @@ const TestCheckBox = () => {
                         <label>Root {o + 1}</label>
                     </div>
                     <ul>
-                        {rootGroup.map((group, g) => {
-                            console.log("group", group)
-                            console.log('rootData.R1', `${rootData.group}`)
+                        {rootGroup[o][set].map((group, g) => {
+
+
                             return (<>
                                 <li>
-                                    <span>qwewewe{group}</span>
+                                    <span>{group}</span>
                                     <input
                                         type="checkbox"
                                         className={set}
-                                        name={`${group}`}
-
+                                        name={`${set}`}
+                                        id={`${group}_${set}`}
                                         onChange={(event) => toggleCheck(event)}
-                                        checked={rootData.R1}
+                                        checked={rootData[`${group}_${set}`]}
                                     />
                                     <label> R {o + 1} {g + 1}</label>
                                     <ul>
@@ -119,7 +136,7 @@ const TestCheckBox = () => {
                                                 name="One"
                                                 data-service={`ip`}
                                                 onChange={(event) => toggleLeaf(event)}
-                                                checked={rootData.One_service?.ip}
+                                                checked={rootData[`${group}_${set}_service`]?.ip}
                                             />
                                             <label>One of one</label>
 
@@ -131,7 +148,7 @@ const TestCheckBox = () => {
                                                 name="One"
                                                 data-service={`op`}
                                                 onChange={(event) => toggleLeaf(event)}
-                                                checked={rootData.One_service?.op}
+                                                checked={rootData[`${group}_${set}_service`]?.op}
                                             />
                                             <label>One of two</label>
 
