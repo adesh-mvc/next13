cc1b5488802eaabd93f873cdadae58cc5175626c
adminmojo <adesh.mvc@gmail.com>
2023-06-29 16:56:27 +0530

    month and year implemented in easepick

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -2,6 +2,8 @@
 
 import { useEffect } from "react"
 import { easepick } from "@easepick/core";
+import { AmpPlugin } from "@easepick/amp-plugin";
+import { RangePlugin } from "@easepick/range-plugin";
 import { useState } from "react";
 
 const pickerData = (inputId, stateSetterFunction) => {
@@ -9,7 +11,20 @@ const pickerData = (inputId, stateSetterFunction) => {
         element: document.getElementById(inputId),
         css: [
             'https://cdn.jsdelivr.net/npm/@easepick/core@1.2.1/dist/index.css',
+            'https://cdn.jsdelivr.net/npm/@easepick/range-plugin@1.2.1/dist/index.css',
         ],
+        plugins: [AmpPlugin],
+
+        AmpPlugin: {
+            dropdown: {
+                months: true,
+                years: true,
+            },
+        },
+        // plugins: [RangePlugin],
+        // RangePlugin: {
+        //     tooltip: true,
+        // },
         format: "DD/MM/YYYY",
         setup(picker) {
             picker.on('select', (e) => {
@@ -39,14 +54,21 @@ export default function ({ params }) {
         console.log("DatepickerChangevALUE:", pickerDate)
     }, [pickerDate])
     useEffect(() => {
-        pickerData('d1', DatepickerChange);
+        const startDate = pickerData('d1', DatepickerChange);
+        startDate.setDate("30/06/2023");
         pickerData('d2', DatepickerChange);
     }, [])
 
     return (<>
         <form >
             <input type="text" name="d1" placeholder="d1" defaultValue={pickerDate.d1} id="d1" />
+            <br />
+            <br />
             <input type="text" name="d2" placeholder="D 2" defaultValue={pickerDate.d2} id="d2" />
-            <input type="text" name="d_3" onChange={(e) => DatepickerChange('d_3', e.target.value)} placeholder="D 2" defaultValue={pickerDate.d3} id="d_3" />
+            <br />
+            <br />
+            <input type="text" name="d_3" onChange={(e) => DatepickerChange('d_3', e.target.value)} placeholder="D 3" defaultValue={pickerDate.d3} id="d_3" />
+            <br />
+            <br />
         </form>    </>)
 }
